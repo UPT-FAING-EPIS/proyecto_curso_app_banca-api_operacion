4141e14cf61f55cd2106156c65535cdd3ca24b1b
Alex Arhuata T <alearhuatat@upt.pe>
2023-04-28 22:46:05 -0500

    Creación del proyecto

---
+++ src/main/java/dev/banco/operaciones/OperacionesApplication.java
@@ -0,0 +1,13 @@
+package dev.banco.operaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperacionesApplication.class, args);
+	}
+
+}
