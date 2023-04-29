c4b62585502ba7245b1bf4ffe9b38e2c846f84af
Brian Rojas <72364530+Brian-Rojas-B@users.noreply.github.com>
2023-04-29 14:42:33 -0500

    v3Deposito

---
+++ src/main/java/dev/banco/operaciones/models/Deposito.java
@@ -0,0 +1,30 @@
+package dev.banco.operaciones.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class Deposito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String numeroCuenta;
+    private BigDecimal monto;
+    @CreationTimestamp
+    private LocalDateTime fechaHoraTransaccion;
+}
